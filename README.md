6544a5694d096f417c5d059c08407a4443bbe49c
Aaron Fink <afink.2020@saintannsny.org>
2018-12-30 19:20:57 -0500

    first pass translation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Neighborhood Objects
+
+We're going to practice defining classes and creating instances of those classes. Open up the neighborhood.py file and let's get started.
---
+++ neighborhood.py
@@ -0,0 +1,49 @@
+# Level 1:
+# Create a class called Student that has two attributes: a name, and a grade.
+
+
+
+# Now create instances of three different students (student1, student2, and student3).
+
+
+
+# Confirm that the class works by printing out the first student's name.
+
+
+
+# Level 2:
+# Create a class called School that has three attributes: a name, a type, and a size.
+
+
+
+# Create instances of three individual schools.
+
+
+
+# Confirm that the class works by printing out the name and size of the third school.
+
+
+
+# Level 3:
+# Create a class called House that has four attributes: an address, a size, a price, and a number of bedrooms.
+
+
+
+# Create instances of at least three individual houses using.
+
+
+
+# Confirm that the class works by printing out the name and size of the second house.
+
+
+
+# Level 4:
+# Put your three students in a list called my_students, your houses in a list for houses, and your schools in a list for schools.
+
+
+
+# Iterate over the student list, printing out "_____ is in grade __." For each of the students.
+
+
+
+# Iterate over the houses list and print out a description for each one. Do the same for your schools lista.
